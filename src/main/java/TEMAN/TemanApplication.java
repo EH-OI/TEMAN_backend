286485eeac174c960f6e3916981b6c5e9a9bdbb3
EH-OI <kimtae21004@naver.com>
2026-03-24 13:13:21 +0900

    Initial commit

---
+++ src/main/java/TEMAN/TemanApplication.java
@@ -0,0 +1,13 @@
+package TEMAN;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TemanApplication.class, args);
+	}
+
+}
